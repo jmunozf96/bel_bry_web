d8a4428d2ae02ac05b8fa2325e9c5eda9a9c1ef5
jmunozf96 <56194124+jmunozf96@users.noreply.github.com>
2019-10-05 17:22:54 +0200

    Autenticacion

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
+    protected $table = 'SEG_USUARIOS';
 
     /**
      * The attributes that are mass assignable.
